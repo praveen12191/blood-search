a5470103cc0a085c65424658bb9f530d02df2134
praveen12191 <haripraveen816@gmail.com>
2023-07-04 12:37:59 +0530

    Initial commit Edited

--- src/components/PatentsDetails.js
+++ src/components/PatentsDetails.js
@@ -8,6 +8,9 @@ const PatentsDetails = ({details}) => {
                 <div className="details">
                      <h5>Name:{i.name}</h5>
                      <p>Age:{i.age}</p>
+                     <p>Group:{i.group}</p>
+                     <p>Place:{i.place}</p>
+                     <h3>Phone:{i.phone}</h3>
                      <button>Done</button>
                 </div>
             </div>
